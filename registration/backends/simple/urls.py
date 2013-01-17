31e4eac3be5d06db91c82104a6594a0b6c49466b
Teddy Knox <teddyknox@gmail.com>
2013-01-17 16:45:19 -0500

    integrated django-registration into the project

---
+++ registration/backends/simple/urls.py
@@ -0,0 +1,45 @@
+"""
+URLconf for registration and activation, using django-registration's
+one-step backend.
+
+If the default behavior of these views is acceptable to you, simply
+use a line like this in your root URLconf to set up the default URLs
+for registration::
+
+    (r'^accounts/', include('registration.backends.simple.urls')),
+
+This will also automatically set up the views in
+``django.contrib.auth`` at sensible default locations.
+
+If you'd like to customize the behavior (e.g., by passing extra
+arguments to the various views) or split up the URLs, feel free to set
+up your own URL patterns for these views instead.
+
+"""
+
+
+from django.conf.urls.defaults import *
+
+from registration.views import activate
+from registration.views import register
+
+def template_view(template_name):
+    try:
+        from django.views.generic.simple import direct_to_template
+        def curried_template_view(request, extra_context=None, mimetype=None, **kwargs):
+            return direct_to_template(request, template_name, extra_context, mimetype, **kwargs)
+        return curried_template_view
+    except ImportError:
+        from django.views.generic import TemplateView
+        return TemplateView.as_view(template_name=template_name)
+
+urlpatterns = patterns('',
+                       url(r'^register/$',
+                           register,
+                           {'backend': 'registration.backends.simple.SimpleBackend'},
+                           name='registration_register'),
+                       url(r'^register/closed/$',
+                           template_view('registration/registration_closed.html'),
+                           name='registration_disallowed'),
+                       (r'', include('registration.auth_urls')),
+                       )
--- static_pages/urls.py
+++ static_pages/urls.py
@@ -1,11 +1,21 @@
 from django.conf.urls import patterns, url
-from django.views.generic.simple import direct_to_template
+
+# for django 1.4.x support
+def template_view(template_name):
+    try:
+        from django.views.generic.simple import direct_to_template
+        def curried_template_view(request, extra_context=None, mimetype=None, **kwargs):
+            return direct_to_template(request, template_name, extra_context, mimetype, **kwargs)
+        return curried_template_view
+    except ImportError:
+        from django.views.generic import TemplateView
+        return TemplateView.as_view(template_name=template_name)
 
 # serving up static pages with RequestContext variables
 urlpatterns = patterns('',
-    url(r'^$', direct_to_template, {'template': 'home.html'}, name='home'),
-	url(r'^help/$', direct_to_template, {'template':'help.html'}, name='help'),
-	url(r'^contact/$', direct_to_template, {'template':'contact.html'}, name='contact'),
-	url(r'^faq/$', direct_to_template, {'template':'faq.html'}, name='faq'),
-	url(r'^about/$', direct_to_template, {'template':'about.html'}, name='about'),
+    url(r'^$', template_view('home.html'), name='home'),
+	url(r'^help/$', template_view('help.html'), name='help'),
+	url(r'^contact/$', template_view('contact.html'), name='contact'),
+	url(r'^faq/$', template_view('faq.html'), name='faq'),
+	url(r'^about/$', template_view('about.html'), name='about'),
 )
