7c7edd5c6ab832244d44a31de9fc4c5ede8740e9
Teddy Knox <teddyknox@gmail.com>
2013-03-09 18:20:38 -0500

    trying to get client ip with heroku.

--- rocketlistings/__init__.py
+++ rocketlistings/__init__.py
@@ -5,6 +5,8 @@ def get_client_ip(request):
 	ip_adds = request.environ.get('HTTP_X_FORWARDED_FOR')
 	if ip_adds:
 		ip_adds = request.environ.get('HTTP_X_FORWARDED_FOR').split(",")   
+		print "IP ADDRESS", ip_adds[0]
 		return ip_adds[0]
 	else:
+		print "IP ADDRESS", ip_adds[0]		
 		return request.META['REMOTE_ADDR']
