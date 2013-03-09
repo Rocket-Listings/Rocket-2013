39c9cb02c330121bca11859816745b1bd5908b24
Teddy Knox <teddyknox@gmail.com>
2013-03-09 17:44:12 -0500

    updated s3storages.py

--- rocketlistings/s3storages.py
+++ rocketlistings/s3storages.py
@@ -1,2 +1,3 @@
+from storages.backends.s3boto import S3BotoStorage
 StaticStorage = lambda: S3BotoStorage(bucket='static.rocketlistings.com')
 MediaStorage  = lambda: S3BotoStorage(bucket='media.rocketlistings.com')
