ff44c0f23f8c369bdbf10ce2bb26762a86b2aebb
Teddy Knox <teddyknox@gmail.com>
2013-03-12 00:14:32 -0400

    got working all of the things.

--- rocketlistings/__init__.py
+++ rocketlistings/__init__.py
@@ -4,9 +4,9 @@ from django.conf import settings
 def get_client_ip(request):
 	ip_adds = request.environ.get('HTTP_X_FORWARDED_FOR')
 	if ip_adds:
-		ip_adds = request.environ.get('HTTP_X_FORWARDED_FOR').split(",")   
-		print "HTTP_X_FORWARDED_FOR", ip_adds[0]
+		ip_adds = ip_adds.split(",")   
+#		print "HTTP_X_FORWARDED_FOR", ip_adds[0]
 		return ip_adds[0]
 	else:
-		print "REMOTE_ADDR", ip_adds[0]		
+		print "REMOTE_ADDR", request.META['REMOTE_ADDR']
 		return request.META['REMOTE_ADDR']
--- rocketlistings/urls.py
+++ rocketlistings/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
 
 admin.autodiscover()
 
@@ -12,6 +13,6 @@ urlpatterns = patterns('',
 	url(r'^users/', include('users.urls')),
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'^search/', include('haystack.urls')),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
