737ecad7d4fe4152f0eccd8b96051939275a2da2
Teddy Knox <teddyknox@gmail.com>
2013-03-11 20:48:32 -0400

    THE D IS SILENT. VERSION 1.5 MUTHA

--- rocketlistings/urls.py
+++ rocketlistings/urls.py
@@ -6,11 +6,11 @@ from django.conf.urls.static import static
 admin.autodiscover()
 
 urlpatterns = patterns('',
+	url(r'', include('static_pages.urls')),
 	url(r'^listings/', include('listings.urls')),
 	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 	url(r'^users/', include('users.urls')),
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'^search/', include('haystack.urls')),
-	url(r'', include('static_pages.urls')),
 	url(r'^grappelli/', include('grappelli.urls')),
 )
