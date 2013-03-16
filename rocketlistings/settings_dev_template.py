7cd0667c6db6b6d26aa575669ff2880bb8b014d5
Teddy Knox <teddyknox@gmail.com>
2013-03-16 15:39:19 -0400

    Updated development settings template.

--- rocketlistings/settings_dev_template.py
+++ rocketlistings/settings_dev_template.py
@@ -1,27 +1,58 @@
+import os
+
+# General settings
 DEBUG = True
+PREPEND_WWW = False
 
-DB_ENGINE = 'django.db.backends.postgresql_psycopg2' 
-DB_NAME = 'teddyknox' 
-DB_USER = 'teddyknox' 
-DB_PASSWORD = '' # blank by default
-DB_HOST = '' # Keep empty string for localhost. 
-DB_PORT = '' # Keep empty string for default. 
+# Hosts/domain names that are valid for this site; required if DEBUG is False
+# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = ['*']
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
+# Database settings
+DB = {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2' , # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
 
+
+# file storage settings
+SITE_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+STATIC_ROOT = os.path.join(SITE_ROOT, 'static_collected/')
+MEDIA_ROOT = os.path.join(SITE_ROOT, 'media/')
+
+UPLOAD_DIR = 'uploads'
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+UPLOAD_URL = MEDIA_URL + 'uploads/'
+
+# Logging settings
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
     'filters': {
         'require_debug_false': {
             '()': 'django.utils.log.RequireDebugFalse'
         }
     },
     'handlers': {
+        'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
         'mail_admins': {
             'level': 'ERROR',
             'filters': ['require_debug_false'],
@@ -30,7 +61,7 @@ LOGGING = {
     },
     'loggers': {
         'django.request': {
-            'handlers': ['mail_admins'],
+            'handlers': ['mail_admins', 'console'],
             'level': 'ERROR',
             'propagate': True,
         },
