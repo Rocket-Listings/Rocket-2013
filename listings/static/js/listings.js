177031d151ef5f48967257d84785b6b9fe3d6166
nkelner <nathkelner@gmail.com>
2013-03-19 17:58:09 -0400

    hopefully fixed copy pasta

--- listings/static/js/listings.js
+++ listings/static/js/listings.js
@@ -1,5 +1,5 @@
 $(function() {
-	ZeroClipboard.setDefaults({moviePath: STATIC_URL+'js/ZeroClipboard.swf'});
+	ZeroClipboard.setDefaults({moviePath: '/static/js/ZeroClipboard.swf'});
 	var clip = new ZeroClipboard( $(".clipboard") );
 
 	var photoId = parseInt((window.location.hash || "").substring(1));
