b94062053b5178c841b7f9b0179abc5f2c034caf
nkelner <nathkelner@gmail.com>
2013-03-22 20:16:50 -0400

    moar copy pasta

--- users/static/js/users.js
+++ users/static/js/users.js
@@ -1,8 +1,14 @@
 $(function() {
 	ZeroClipboard.setDefaults({moviePath: STATIC_URL +'js/ZeroClipboard.swf', 
-								trustedDomains: ['s3.amazonaws.com'], 
-								allowScriptAccess: 'always'});
+								trustedDomains: [   'beta.rocketlistings.com', 
+                    								'rocketlistings.com', 
+                    								'www.rocketlistings.com', 
+                    								'rocket-listings.herokuapp.com',
+                    								'quiet-beyond-7797.herokuapp.com'
+                    						]});
 	var clip = new ZeroClipboard( $(".clipboard") );
+	clip.on( 'mousedown', function(client){ $(this).addClass("active"); })
+	clip.on( 'mouseup', function(client){ $(this).removeClass("active"); });
 /*	function getURLParameter(name) {
 		return decodeURI(
 			(RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
