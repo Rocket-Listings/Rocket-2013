da19229eef1bc512fc38d86507d1f49a07a9d78e
Teddy Knox <teddyknox@gmail.com>
2013-03-24 18:47:28 -0400

    disabled gevent for postgres

--- gunicorn.py
+++ gunicorn.py
@@ -10,9 +10,9 @@ sys.path.append(SITE_ROOT / 'apps')
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "gevent"
 
-def def_post_fork(server, worker):
-    from rocketlistings.psyco_gevent import make_psycopg_green
-    make_psycopg_green()
-    worker.log.info("Made Psycopg Green")
+# def def_post_fork(server, worker):
+#     from rocketlistings.psyco_gevent import make_psycopg_green
+#     make_psycopg_green()
+#     worker.log.info("Made Psycopg Green")
 
-post_fork = def_post_fork
+# post_fork = def_post_fork
