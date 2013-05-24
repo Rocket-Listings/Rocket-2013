5b35fb18278d08e2d8264e19a6b09b58196da068
nkelner <nathkelner@gmail.com>
2013-05-24 16:43:37 -0400

    alpha recieving mail and saving it to message thread seems to work..

--- apps/mail/views.py
+++ apps/mail/views.py
@@ -1,5 +1,5 @@
 from mail.models import mailgun
-from listings.models import Message
+from listings.models import Message, Buyer, Listing
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 import hashlib, hmac
@@ -7,6 +7,7 @@ from django.core.mail import send_mail
 from django.contrib.auth.models import User
 from users.models import UserProfile
 from django.shortcuts import render, redirect, get_object_or_404
+import re
 
 
 
@@ -15,6 +16,11 @@ from django.shortcuts import render, redirect, get_object_or_404
 # Encoding the resulting string with the HMAC algorithm (using your API Key as a key and SHA256 digest mode).
 # Comparing the resulting hexdigest to the signature.
 def verify(token, timestamp, signature):
+	"""this function secures the webhook by:
+	Concatenating timestamp and token values.
+	Encoding the resulting string with the HMAC algorithm (using your API Key as a key and SHA256 digest mode).
+	Comparing the resulting hexdigest to the signature."""
+
 	api_key = 'key-9flqj538z-my-qcnpc74c2wit4vibl-3'
 	return signature == hmac.new(
                              key=api_key,
@@ -38,25 +44,65 @@ def on_incoming_message(request):
 		token = request.POST.get('token', '')
 		sig = request.POST.get('signature', '')
 
-	if str(sender).partition('@')[2] == 'craigslist.org':
-		print "is @craigslist"
-		name = recipient.split('@')[0]
-		user = get_object_or_404(User, username=name)
-		#print user.listing_set.get(title__exact= subject.partition('"')[2].partition('"')[0])
-		listing = user.listing_set.get(title__exact= subject.partition('"')[2].partition('"')[0])
-		#listing = get_object_or_404(Listing, user = user, )
-		email = user.email
-		#print subject.partition('"')[2].partition('"')[0]
-		print send_mail( subject , body, 'postmaster@rocketlistings.mailgun.org', [email], fail_silently=False)
-		#return HttpResponse('OK')
-
+	
 	if verify(token, timestamp, sig):
 		print "verified"
-		message = Message(listing = listing, content = body, buyer = listing.buyer_set.get(name__exact = "Craigslist"))
-		message.save()
-		m = mailgun(recipient = recipient, sender = sender, frm = frm, subject = subject, body = body, text = text, 
-		signature = signature, timestamp = timestamp, token = token, sig = sig)
-		m.save()
-		return HttpResponse('OK')
+
+		if str(sender).startswith('bounce-post'):
+			"""Handles administrative emails from craigslist"""
+
+			print "is @craigslist"
+			print sender
+			print frm
+			return HttpResponse('OK')
+			user = get_object_or_404(User, username= recipient.split('@')[0])
+			listing = user.listing_set.get(title__exact= subject.partition('"')[2].partition('"')[0])
+			email = user.email
+			print send_mail( subject , body, 'rocket@rocketlistings.mailgun.org', [email], fail_silently=False)
+
+			message = Message(listing = listing, content = body, buyer = listing.buyer_set.get(name__exact = "Craigslist"))
+			message.save()
+			m = mailgun(recipient = recipient, sender = sender, frm = frm, subject = subject, body = body, text = text, 
+			signature = signature, timestamp = timestamp, token = token, sig = sig)
+			m.save()
+
+			return HttpResponse('OK')
+
+		elif str(sender).partition('@')[2] == 'example.com': #temporary to test  webhook
+			m = mailgun(recipient = recipient, sender = sender, frm = frm, subject = subject, body = body, text = text, 
+			signature = signature, timestamp = timestamp, token = token, sig = sig)
+			m.save()
+			return HttpResponse('OK')
+
+		elif str(sender).startswith('bounce-anon'):
+			"""Handles emails from anonomized cl buyers"""
+
+			print "is @sale.craigslist.org"
+			user = get_object_or_404(User, username= recipient.split('@')[0])
+			listing = user.listing_set.get(title__exact= subject.partition(' - ')[0])
+			buyer = str(re.findall(r'"(.*?)"', frm))
+			buyer_email = str(re.findall(r'<(.*?)>', frm))
+
+			buyers = listing.buyer_set.all()
+			for buy in buyers:
+				if buy.name == buyer:
+					break
+			else:
+				b = Buyer(listing = listing, name = buyer, email = buyer_email)#create a "buyer" to recieve cl messages
+				b.save()
+
+			print body, buyer
+			
+
+			message = Message(listing = listing, content = body, buyer = listing.buyer_set.get(name__exact = buyer))
+			message.save()
+			m = mailgun(recipient = recipient, sender = sender, frm = frm, subject = subject, body = body, text = text, 
+			signature = signature, timestamp = timestamp, token = token, sig = sig)
+			m.save()
+
+			return HttpResponse('OK')
+
+
 	else:
+		print "didnt work"
 		return HttpResponse('Unauthorized')
