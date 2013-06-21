3c87da1bea135c6fd29721495ded0231ad7c1acb
nkelner <nathkelner@gmail.com>
2013-06-21 09:53:22 -0400

    begining to refactor the mail app

--- apps/mail/views.py
+++ apps/mail/views.py
@@ -27,90 +27,38 @@ def on_incoming_message(request):
 		print "post recieved"
 
 		sender    = request.POST.get('sender')
-		print sender, len(sender)
+		print sender
 
 		recipient = request.POST.get('recipient')
-		print recipient, len(recipient)
+		print recipient
 
 		subject   = request.POST.get('subject', '')
-		print subject, len(subject)
+		print subject
 
 		frm = request.POST.get('from', '')
-		print frm, len(frm)
+		print frm
 
 		body = request.POST.get('body-plain', '')
-		body_html = request.POST.get('body-html', '')
-		text = request.POST.get('stripped-text', '')
+		print body
 
+		body_html = request.POST.get('body-html', '')
+		print body_html
+
+		text = request.POST.get('stripped-text', '')
+		print text
 
 		signature = request.POST.get('stripped-signature', '')
+		print signature
 		timestamp = request.POST.get('timestamp', '')
 		token = request.POST.get('token', '')
 		sig = request.POST.get('signature', '')
-		print signature, len(signature)
+
 
 	
 	if verify(token, timestamp, sig):
 		print "verified"
-
-		if str(sender).startswith('bounce-post'):
-			"""Handles administrative emails from craigslist"""
-
-			print "is @craigslist"
-			print sender
-			print frm
-			user = get_object_or_404(User, username= recipient.split('@')[0])
-			listing = user.listing_set.get(title__exact= subject.partition('"')[2].partition('"')[0])
-			email = user.email
-			print send_mail( subject , body, 'rocket@rocketlistings.mailgun.org', [email], fail_silently=False)
-
-			message = Message(listing = listing, content = body, buyer = listing.buyer_set.get(name__exact = "Craigslist"))
-			message.save()
-			m = mailgun(recipient = recipient, sender = sender, frm = frm, subject = subject, body = body, text = text, 
-			signature = signature, timestamp = timestamp, token = token, sig = sig)
-			m.save()
-
-			return HttpResponse('OK')
-
-		elif str(sender).partition('@')[2] == 'example.com': #temporary to test  webhook
-			m = mailgun(recipient = recipient, sender = sender, frm = frm, subject = subject, body = body, text = text, 
-			signature = signature, timestamp = timestamp, token = token, sig = sig)
-			m.save()
-			return HttpResponse('OK')
-
-		elif str(sender).startswith('bounce-anon'):
-			"""Handles emails from anonomized cl buyers"""
-
-			print "is @sale.craigslist.org"
-			user = get_object_or_404(User, username= recipient.split('@')[0])
-			listing = user.listing_set.get(title__exact= subject.partition(' - ')[0])
-			buyer = re.findall(r'"(.*?)"', frm)
-			buyer_email = re.findall(r'<(.*?)>', frm)
-			print type(buyer)
-
-			buyers = listing.buyer_set.all()
-
-			for buy in buyers:
-				print buy.name == str(buyer)
-				
-				if buy.name == str(buyer):
-					break
-			else:
-				b = Buyer(listing = listing, name = buyer)#create a "buyer" to recieve cl messages
-				b.save()
-
-			print body, buyer
-			
-
-			message = Message(listing = listing, content = body, buyer = listing.buyer_set.get(name__exact = buyer))
-			message.save()
-			m = mailgun(recipient = recipient, sender = sender, frm = frm, subject = subject, body = body, text = text, 
-			signature = signature, timestamp = timestamp, token = token, sig = sig)
-			m.save()
-
-			return HttpResponse('OK')
-
+		return HttpResponse('OK')
 
 	else:
-		print "didnt work"
+		print "not verified"
 		return HttpResponse('Unauthorized')
