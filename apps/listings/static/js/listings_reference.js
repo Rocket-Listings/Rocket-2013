cfd46184069e8fe70a1b82a8e8eb6789a1dd19c0
Katsa <gykatsaounis@gmail.com>
2013-06-24 15:45:35 -0400

    committing specs, on category page

--- apps/listings/static/js/listings_reference.js
+++ apps/listings/static/js/listings_reference.js
@@ -1,13 +1,31 @@
 $(document).ready(function(){
-
+	var prev = "";
 	function eventHandlers() {
 		$(".for_sale").click(handleForSaleClick);
 		$(".housing").click(handleHousingClick);
 	}
 
 	function handleForSaleClick(e){
+		var val = ($(this).attr("data-id"));
+
 		$("#category").text( "for sale >> " + $(this).html());
 		$("#collapseOne").collapse('hide');
+		if (prev != val ){
+			$('.category_' + prev).hide();
+			$('.category_' + val).show();
+
+			console.log('v'+val);
+			console.log('p'+prev);
+			prev = val;
+			console.log('p'+prev);
+
+		}
+		else{
+		prev = val;
+		console.log(prev);
+		console.log(val);
+		$('.category_' + val).show();
+	}
 	}
 
 	function handleHousingClick(e){
