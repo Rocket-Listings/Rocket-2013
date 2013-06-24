4a12457d1a9255275c02d05aff6fe9922deddd8d
Moore <moorewt@mac.com>
2013-06-24 15:52:44 -0400

    prior to image upload

--- apps/listings/static/js/listings_reference.js
+++ apps/listings/static/js/listings_reference.js
@@ -8,12 +8,38 @@ $(document).ready(function(){
 	function handleForSaleClick(e){
 		$("#category").text( "for sale >> " + $(this).html());
 		$("#collapseOne").collapse('hide');
+		$(".edit").show();
+
 	}
 
 	function handleHousingClick(e){
 		$("#category").text("housing >> " + $(this).html());
 		$("#collapseTwo").collapse('hide');
+		$(".edit").show();
 	}
 
 	eventHandlers();
 });
+
+function FilePickClick(){
+  filepicker.pickMultiple({
+      mimetypes: ['image/*', 'text/plain'],
+      services:['COMPUTER', 'URL'],
+    },
+    function(InkBlob){
+      console.log(JSON.stringify(InkBlob));
+    },
+    function(FPError){
+      console.log(FPError.toString());
+    }
+  );
+};
+
+function fileUpload(){
+	filepicker.pickAndStore({
+		mimetype:"image/*"},
+	    {location:"S3"}, 
+	    function(InkBlobs){
+	   		console.log(JSON.stringify(InkBlobs));
+	});
+};
