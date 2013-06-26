6fa346d2142d4bda273dfe0b25d4aec65b18bbdf
Dev Machine <devmachine@Devs-MacBook-Pro.local>
2013-06-26 18:02:41 -0400

    made it so swapping from edit to preview reflects the changes in edit without ajax.

--- apps/listings/static/js/listings_reference.js
+++ apps/listings/static/js/listings_reference.js
@@ -88,5 +88,9 @@ function Preview(){
       $(document).ready(function(){
         $(".edit").hide();
         $(".listing").show();
+        $('.l-description').text($('#id_description').val());
+		$('.title').text($('#id_title').val());
+		$('.l-location').text("(" + $('#id_location').val() + ")");
+		$('.l-price').text("$" + $('#id_price').val());
     });
 }
