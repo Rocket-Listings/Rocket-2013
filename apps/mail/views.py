bc4cdafbd63d7fbe93082cc4b5707518150d497a
nkelner <nathkelner@gmail.com>
2013-06-26 11:28:07 -0400

    Almost got all incoming email squared away

--- apps/mail/views.py
+++ apps/mail/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import User
 from users.models import UserProfile
 from django.shortcuts import render, redirect, get_object_or_404
 import re
+from django.core.exceptions import ObjectDoesNotExist
 
 def verify(token, timestamp, signature):
 	"""this function secures the webhook by:
@@ -80,29 +81,57 @@ def on_incoming_buyer_message(request):
 
 	if request.method == 'POST':
 		print "buyer post recieved"
-		mime = request.POST.get('message-headers')
-		print mime
-
-
-		sender = request.POST.get('sender')
-		print sender
-		recipient = request.POST.get('recipient')
-		print recipient
-		subject   = request.POST.get('subject', '')
-		print subject
-		frm = request.POST.get('from', '')
-		print frm.partition('"')[2].partition('"')[0]
-		body = request.POST.get('body-plain', '')
-		print body
-		body_html = request.POST.get('body-html', '')
-		print body_html
-		text = request.POST.get('stripped-text', '')
-		print text
-		signature = request.POST.get('stripped-signature', '')
-		print signature
-
-		user = get_object_or_404(User, username= request.POST.get('recipient').split('@')[0])
 		
+		user = get_object_or_404(User, username= request.POST.get('recipient').split('@')[0])
+		listing = user.listing_set.get(title__exact= request.POST.get('subject', '').partition(' - ')[0])
+
+		print request.POST.get('message-headers')
+
+		buyer_name = request.POST.get('from', '').partition("\"")[2].partition("\"")[0]
+		buyer_email = request.POST.get('from', '').partition("<")[2].partition(">")[0]
+
+		print buyer_email
+
+		try:
+			b = Buyer.objects.get(listing= listing, name= buyer_name)
+			print "found buyer"
+		except ObjectDoesNotExist:
+			print "buyer not found--creating buyer"
+
+			b = Buyer(listing = listing, name = buyer_name, email = buyer_email)
+			b.save()
+
+		print b
+
+		
+		#message = Message(listing = listing, content = body, buyer = listing.buyer_set.get(name__exact = buyer))
+		#message.save()
+		#m = mailgun(recipient = recipient, sender = sender, frm = frm, subject = subject, body = body, text = text, 
+		#signature = signature, timestamp = timestamp, token = token, sig = sig)
+		#m.save()
+
+
+
+
+		#buyers = listing.buyer_set.all()
+
+		#for buy in buyers:
+			#print buy.name == str(buyer)
+			
+			#if buy.name == str(buyer):
+				#break
+		#else:
+			#b = Buyer(listing = listing, name = buyer)#create a "buyer" to recieve cl messages
+			#b.save()
+
+		#print body, buyer
+		
+
+		#message = Message(listing = listing, content = body, buyer = listing.buyer_set.get(name__exact = buyer))
+		#message.save()
+		#m = mailgun(recipient = recipient, sender = sender, frm = frm, subject = subject, body = body, text = text, 
+		#signature = signature, timestamp = timestamp, token = token, sig = sig)
+		#m.save()
 
 	if verify(request.POST.get('token', ''), request.POST.get('timestamp', ''), request.POST.get('signature', '')):
 		print "verified"
