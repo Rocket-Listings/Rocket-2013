52940394e85f57ddadd14082a142e072f112c5cd
Dev Machine <devmachine@Devs-MacBook-Pro.local>
2013-06-28 15:02:13 -0400

    more styling stuff, added placeholders and edited the location text in relation to the title and such. also, i got rid of the white-space problem.

--- apps/listings/static/js/details.js
+++ apps/listings/static/js/details.js
@@ -10,8 +10,8 @@ function Preview(){
         $(".edit").hide();
         $(".listing").show();
         $('.l-description').text($('#id_description').val());
-		$('.title').text($('#id_title').val());
-		$('.l-location').text("(" + $('#id_location').val() + ")");
-		$('.l-price').text("$" + $('#id_price').val());
+		    $('.title').text($('#id_title').val());
+		    $('.l-location').text("(" + $('#id_location').val() + ")");
+		    $('.l-price').text("$" + $('#id_price').val());
     });
 }
