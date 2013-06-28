91e16010868f15249b5a33a34bbb2b81f51f855d
Dev Machine <devmachine@Devs-MacBook-Pro.local>
2013-06-28 17:09:09 -0400

    added the feature that on page load, the category when you update a listing is selected for you

--- apps/listings/static/js/details.js
+++ apps/listings/static/js/details.js
@@ -1,3 +1,20 @@
+$(document).ready(function(){
+  var category = $('.hidden').val().toLowerCase().trim();
+  $(":contains("+category+")").addClass("selected");
+
+  if (category == 'apartment / housing' || category == 'rooms / shared' || category == 'sublets / temporary' 
+    || category == 'housing wanted' || category == 'housing swap' || category == 'vacation rentals' 
+    || category == 'parking / storage' || category == 'office / commercial' || category == 'real estate for sale') {
+
+    $('#1').removeClass("active");
+    $('#2').addClass("active");
+    $("#tab1").removeClass("active");
+    $("tab2").addClass("active");
+  }
+});
+
+
+
 function Edit(){
       $(document).ready(function(){
         $(".listing").hide();
--- apps/listings/static/js/listings_reference.js
+++ apps/listings/static/js/listings_reference.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
+
 	$('.l-description').text($('#id_description').val());
 	$('.title').text($('#id_title').val());
 	$('.l-location').text("(" + $('#id_location').val() + ")");
 	$('.l-price').text("$" + $('#id_price').val());
+
 	var prev = "";
 	function eventHandlers() {
 		$("a", ".tab-pane").click(handleClick);
