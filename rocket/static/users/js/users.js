e8405bd7ae540efc3675f821e583605a310f5eb5
danasilver <dsilver1221@gmail.com>
2013-07-15 15:04:02 -0400

    facebook groundwork laid, permissions/login in place

--- rocket/static/users/js/users.js
+++ rocket/static/users/js/users.js
@@ -1,3 +1,5 @@
+{% load static from staticfiles %}
+
 $(function() {
 	// USER MANAGEMENT JS
 	filepicker.setKey('ATM8Oz2TyCtiJiHu6pP6Qz');
@@ -218,10 +220,20 @@ $(function() {
 		}
 		errors.show();
 	}
-  	window.fbAsyncInit = function() {
+
+	$('.facebook-connect').click(function() {
+		FB.login(function(response) {
+
+		}, {perms:'email,user_location'});
+	});
+});
+
+//  FACEBOOK
+
+window.fbAsyncInit = function() {
   	FB.init({
     	appId      : '279057228903179', // App ID
-    	// channelUrl : 'file://localhost/Users/olindavis/Rocket-Listings-Django/apps/users/templates/channel.html', // Channel File
+    	channelUrl : '{% static "/users/channel.html" %}', // Channel File
     	status     : true, // check login status
     	cookie     : true, // enable cookies to allow the server to access the session
     	xfbml      : true  // parse XFBML
@@ -237,7 +249,7 @@ $(function() {
       	// The response object is returned with a status field that lets the app know the current
       	// login status of the person. In this case, we're handling the situation where they 
       	// have logged in to the app.
-      	testAPI();
+      	//testAPI();
     	} else if (response.status === 'not_authorized') {
       		// In this case, the person is logged into Facebook, but not into the app, so we call
       		// FB.login() to prompt them to do so. 
@@ -246,37 +258,36 @@ $(function() {
       		// (1) JavaScript created popup windows are blocked by most browsers unless they 
       		// result from direct interaction from people using the app (such as a mouse click)
       		// (2) it is a bad experience to be continually prompted to login upon page load.
-      	FB.login();
+      	//FB.login();
     	} else {
       		// In this case, the person is not logged into Facebook, so we call the login() 
       		// function to prompt them to do so. Note that at this stage there is no indication
       		// of whether they are logged into the app. If they aren't then they'll see the Login
       		// dialog right after they log in to Facebook. 
       		// The same caveats as above apply to the FB.login() call here.
-      	FB.login();
+      	//FB.login();
     	}
   	});
-  	};
+};
 
-  	// Load the SDK asynchronously
-  	(function(d){
-   		var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-   		if (d.getElementById(id)) {return;}
-   		js = d.createElement('script'); js.id = id; js.async = true;
-   		js.src = "//connect.facebook.net/en_US/all.js";
-   		ref.parentNode.insertBefore(js, ref);
-  	}(document));
+// Load the SDK asynchronously
+(function(d){
+	var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+	if (d.getElementById(id)) {return;}
+	js = d.createElement('script'); js.id = id; js.async = true;
+	js.src = "//connect.facebook.net/en_US/all.js";
+	ref.parentNode.insertBefore(js, ref);
+}(document));
 
   	// Here we run a very simple test of the Graph API after login is successful. 
   	// This testAPI() function is only called in those cases. 
-  	function testAPI() {
-    	console.log('Welcome!  Fetching your information.... ');
-    	FB.api('/me', function(response) {
-    		console.log('Good to see you, ' + response.name + '.');
-    	});
-  	}
+function testAPI() {
+	console.log('Welcome!  Fetching your information.... ');
+	FB.api('/me', function(response) {
+		console.log('Good to see you, ' + response.name + '.');
+	});
+}
   	function collect_facebook_info() {
   		
   	}
-});
 
