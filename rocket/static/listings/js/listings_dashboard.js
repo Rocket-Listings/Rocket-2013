351900181ad8c75d01dcb4a643e5c8491df1ac8f
David Sander <legodps@yahoo.com>
2013-07-15 10:50:41 -0400

    allowed highlighting of listings on the dashboard.

--- rocket/static/listings/js/listings_dashboard.js
+++ rocket/static/listings/js/listings_dashboard.js
@@ -2,7 +2,8 @@ $(function() {
 
 	$('.table-listings tbody tr').click(function(event) {
 		var listingRow = $(this);
-
+		$('.table-listings tbody tr').removeClass('highlight');
+		listingRow.addClass('highlight');
 		var id = listingRow.data('listing-id');
 		$(".message").hide();
 		$(".buyer-card").hide();
@@ -17,6 +18,7 @@ $(function() {
 	});
 
 	$('.buyer-card').click(function(event){
+		alert("bog");
 		var buyerCard = $(this);
 		$('.buyer-card').removeClass('highlight');
 		buyerCard.addClass('highlight');
