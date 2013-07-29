686587be6b1b893f7250c2db302b1807292f885d
David Sander <legodps@yahoo.com>
2013-07-29 13:55:54 -0400

    moved the documentation for the decorator and removed dashboard.html

--- rocket/apps/users/decorators.py
+++ rocket/apps/users/decorators.py
@@ -15,13 +15,6 @@ def first_visit(view_func):
 	return _wrapped_visit_func
 
 
-
-"""view_count function is used to keep track of the number of times an anonymous or a non-owner of a page 
-views a certain page of another user. It will not add to the page views if the owner of the page visits the page.
-To use this functionality, you need to include set the property of request.user.skip_count to a boolean. 
-The way it has been done is that if request.user is equal to the owner of the page being visited skip_count is set to True
-otherwise it is set to false. Also the decorator needs to be called at the beginning of the function.
-"""
 def view_count(view_func):
 	def _wrapped_visit_func(request, *args, **kwargs):
 		response = view_func(request, *args, **kwargs)
@@ -34,3 +27,10 @@ def view_count(view_func):
 		return response
 
 	return _wrapped_visit_func
+
+"""view_count function is used to keep track of the number of times an anonymous or a non-owner of a page 
+views a certain page of another user. It will not add to the page views if the owner of the page visits the page.
+To use this functionality, you need to include set the property of request.user.skip_count to a boolean. 
+The way it has been done is that if request.user is equal to the owner of the page being visited skip_count is set to True
+otherwise it is set to false. Also the decorator needs to be called at the beginning of the function.
+"""
