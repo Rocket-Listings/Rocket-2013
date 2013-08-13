d8a10d5b0ad6e1003789468e5a505244c3bc743b
Teddy Knox <teddyknox@gmail.com>
2013-08-13 15:07:19 -0400

    enabed better storage backend

--- rocket/settings/storage.py
+++ rocket/settings/storage.py
@@ -1,7 +1,5 @@
 from django.contrib.staticfiles.storage import CachedFilesMixin
-
-from pipeline.storage import PipelineMixin
-
+from pipeline.storage import PipelineMixin, NonPackagingMixin
 from storages.backends.s3boto import S3BotoStorage
 
 
