8f3162f7d08366ef613b94c81ee9e65547668dee
Teddy Knox <teddyknox@gmail.com>
2013-08-14 09:23:07 -0400

    commiting to update heroku

--- rocket/settings/development.py
+++ rocket/settings/development.py
@@ -38,9 +38,15 @@ DATABASES = {
 }
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#caches
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+#     }
+# }
+
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
     }
 }
 
