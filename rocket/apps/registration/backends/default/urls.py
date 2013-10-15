29873ffd97f370f60a48e96491a391b84792f051
Nat Kelner <nathkelner@gmail.com>
2013-10-15 13:22:39 -0400

    Finished the test (which can be run with `./manage.py test users`) and found the activation error bug!

--- rocket/apps/registration/backends/default/urls.py
+++ rocket/apps/registration/backends/default/urls.py
@@ -33,7 +33,7 @@ urlpatterns = patterns('',
                        # confusing 404.
                        url(r'^activate/(?P<activation_key>\w+)/$',
                            activate,
-                           {'backend': 'registration.backends.default.DefaultBackend', 'template_name': 'rocket_registration/activate.html', 'success_url': 'user_profile'},
+                           {'backend': 'registration.backends.default.DefaultBackend', 'template_name': 'rocket_registration/activate.html', 'success_url': 'user_info'},
                            name='registration_activate'),
                        url(r'^register/$',
                            register,
