45ad7ee148eeb64fb4096308203a86a6753f8ba8
berserkia <bsirkia@gmail.com>
2013-11-17 22:49:06 -0500

    got urls updated

--- rocket/apps/static_pages/urls.py
+++ rocket/apps/static_pages/urls.py
@@ -4,8 +4,8 @@ from static_pages.views import index, google_webmaster_verification, home
 # serving up static pages with RequestContext variables
 urlpatterns = patterns('',
     url(r'^$', home, name='home'),
-    url(r'^background/$', index, name='background'),
-    url(r'^about/$', index, name='about'),
+    url(r'^background/$', home, name='background'),
+    url(r'^about/$', home, name='about'),
     # url(r'^pricing/$', 'index', name='pricing'),
     url(r'^googlef43896b8ef9b394c.html', google_webmaster_verification),
 )
