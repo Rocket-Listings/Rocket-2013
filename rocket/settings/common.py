9d7a61a04906b133255871b6057eb7197e744b14
berserkia <bsirkia@gmail.com>
2013-11-17 20:09:28 -0500

    detecting mobile works

--- rocket/settings/common.py
+++ rocket/settings/common.py
@@ -131,6 +131,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#root-urlconf
