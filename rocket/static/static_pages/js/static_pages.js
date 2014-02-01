f3d082c98a10f947ddd049a430e9c563e9f08fed
berserkia <bsirkia@gmail.com>
2014-02-01 12:41:36 -0500

    got the home static pages done, need to add about and pricing pages back in

--- rocket/static/static_pages/js/static_pages.js
+++ rocket/static/static_pages/js/static_pages.js
@@ -1,106 +1,106 @@
 $(function() {
-  google.maps.visualRefresh = true;
-  var mapTypeStyle = [
-    {
-      "featureType": "poi",
-      "elementType": "labels",
-      "stylers": [
-        { "visibility": "off" }
-      ]
-    },{
-      "featureType": "administrative.neighborhood",
-      "elementType": "labels",
-      "stylers": [
-        { "visibility": "off" }
-      ]
-    },{
-      "featureType": "water",
-      "elementType": "labels",
-      "stylers": [
-        { "visibility": "off" }
-      ]
-    },{
-      "featureType": "transit",
-      "elementType": "labels",
-      "stylers": [
-        { "visibility": "off" }
-      ]
-    },{
-      "featureType": "road.highway",
-      "stylers": [
-        { "weight": 1.4 }
-      ]
-    },{
-      "featureType": "road",
-      "elementType": "labels.text",
-      "stylers": [
-        { "visibility": "off" }
-      ]
-    },{
-      "featureType": "water",
-      "stylers": [
-        { "saturation": -44 },
-        { "lightness": -18 },
-        { "hue": "#00ccff" }
-      ]
-    },{
-      "featureType": "road"  },{
-      "featureType": "road.arterial",
-      "stylers": [
-        { "weight": 1.4 }
-      ]
-    },{
-      "featureType": "road.arterial",
-      "elementType": "labels",
-      "stylers": [
-        { "visibility": "off" }
-      ]
-    },{
-      "featureType": "road.local",
-      "elementType": "labels",
-      "stylers": [
-        { "visibility": "off" }
-      ]
-    },{
-      "featureType": "poi.park",
-      "stylers": [
-        { "hue": "#33ff00" }
-      ]
-    },{
-    },{
-      "featureType": "road.highway",
-      "stylers": [
-        { "lightness": 33 }
-      ]
-    }
-  ];
-  var mapOptions = {
-    center: new google.maps.LatLng(44.475, -73.612), // burlington coords 44.5, -72.8
-    zoom: 10,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    disableDefaultUI: true,
-    draggable: false,
-    scrollwheel: false,
-    disableDoubleClickZoom: true,
-    styles: mapTypeStyle
-  };
-  var map = new google.maps.Map(document.getElementById("header-map"), mapOptions);
-  $("#header-map-overlay").css('visibility', 'visible');
+  // google.maps.visualRefresh = true;
+  // var mapTypeStyle = [
+  //   {
+  //     "featureType": "poi",
+  //     "elementType": "labels",
+  //     "stylers": [
+  //       { "visibility": "off" }
+  //     ]
+  //   },{
+  //     "featureType": "administrative.neighborhood",
+  //     "elementType": "labels",
+  //     "stylers": [
+  //       { "visibility": "off" }
+  //     ]
+  //   },{
+  //     "featureType": "water",
+  //     "elementType": "labels",
+  //     "stylers": [
+  //       { "visibility": "off" }
+  //     ]
+  //   },{
+  //     "featureType": "transit",
+  //     "elementType": "labels",
+  //     "stylers": [
+  //       { "visibility": "off" }
+  //     ]
+  //   },{
+  //     "featureType": "road.highway",
+  //     "stylers": [
+  //       { "weight": 1.4 }
+  //     ]
+  //   },{
+  //     "featureType": "road",
+  //     "elementType": "labels.text",
+  //     "stylers": [
+  //       { "visibility": "off" }
+  //     ]
+  //   },{
+  //     "featureType": "water",
+  //     "stylers": [
+  //       { "saturation": -44 },
+  //       { "lightness": -18 },
+  //       { "hue": "#00ccff" }
+  //     ]
+  //   },{
+  //     "featureType": "road"  },{
+  //     "featureType": "road.arterial",
+  //     "stylers": [
+  //       { "weight": 1.4 }
+  //     ]
+  //   },{
+  //     "featureType": "road.arterial",
+  //     "elementType": "labels",
+  //     "stylers": [
+  //       { "visibility": "off" }
+  //     ]
+  //   },{
+  //     "featureType": "road.local",
+  //     "elementType": "labels",
+  //     "stylers": [
+  //       { "visibility": "off" }
+  //     ]
+  //   },{
+  //     "featureType": "poi.park",
+  //     "stylers": [
+  //       { "hue": "#33ff00" }
+  //     ]
+  //   },{
+  //   },{
+  //     "featureType": "road.highway",
+  //     "stylers": [
+  //       { "lightness": 33 }
+  //     ]
+  //   }
+  // ];
+  // var mapOptions = {
+  //   center: new google.maps.LatLng(44.475, -73.612), // burlington coords 44.5, -72.8
+  //   zoom: 10,
+  //   mapTypeId: google.maps.MapTypeId.ROADMAP,
+  //   disableDefaultUI: true,
+  //   draggable: false,
+  //   scrollwheel: false,
+  //   disableDoubleClickZoom: true,
+  //   styles: mapTypeStyle
+  // };
+  // var map = new google.maps.Map(document.getElementById("header-map"), mapOptions);
+  // $("#header-map-overlay").css('visibility', 'visible');
 
-  function getLocationByIP() {
-    $.ajax({
-      method: 'GET',
-      url: 'https://freegeoip.net/json/' + REMOTE_ADDR,
-      success: function(response) {
-        gotLocation(response.latitude, response.longitude);
-      }
-    });
-  }
+  // function getLocationByIP() {
+  //   $.ajax({
+  //     method: 'GET',
+  //     url: 'https://freegeoip.net/json/' + REMOTE_ADDR,
+  //     success: function(response) {
+  //       gotLocation(response.latitude, response.longitude);
+  //     }
+  //   });
+  // }
 
-  function gotLocation(lat, lng) {
-    console.log(lat, lng);
-    map.panTo(new google.maps.LatLng(lat, lng - 0.5));
-  }
+  // function gotLocation(lat, lng) {
+  //   console.log(lat, lng);
+  //   map.panTo(new google.maps.LatLng(lat, lng - 0.5));
+  // }
 
   // getLocationByIP();
 
