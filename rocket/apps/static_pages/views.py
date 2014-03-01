e37f42ccc8841a85f43ec0f640a424129047cea3
Nat Kelner <nathkelner@gmail.com>
2014-02-28 22:12:12 -0500

    Add empty dict to static pages `TemplateResponse`.
    This prevents an occasional attribute error from croping up.

--- rocket/apps/static_pages/views.py
+++ rocket/apps/static_pages/views.py
@@ -23,7 +23,7 @@ def home(request):
 @cache_control(must_revalidate=True, max_age=3600)
 @cache_page(60 * 15)
 def index(request):
-	return TemplateResponse(request, 'static_pages/index.html')
+	return TemplateResponse(request, 'static_pages/index.html', context = {})
 
 def google_webmaster_verification(request):
     return render(request, 'static_pages/googlef43896b8ef9b394c.html')
