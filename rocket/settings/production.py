a1c2a7a5ec23e5ec2846dc0ba09e70e831dce183
Nat Kelner <nathkelner@gmail.com>
2014-03-09 13:31:10 -0400

    Change mail server to mail.rocketlistings.com.

--- rocket/settings/production.py
+++ rocket/settings/production.py
@@ -77,6 +77,6 @@ SOUTH_DATABASE_ADAPTERS = {
 ############# MAILGUN CONFIG
 EMAIL_BACKEND = 'django_mailgun.MailgunBackend'
 MAILGUN_ACCESS_KEY = 'key-9flqj538z-my-qcnpc74c2wit4vibl-3'
-MAILGUN_SERVER_NAME = 'rocketlistings.mailgun.org'
+MAILGUN_SERVER_NAME = 'mail.rocketlistings.org'
 
 AUTOPOST_DEBUG = False
